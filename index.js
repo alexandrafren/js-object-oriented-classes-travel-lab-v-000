d205deb397a5e499b3bded4e1c7b271362e107b2
Alexandra Fren <alexandra.fren@gmail.com>
2018-12-12 19:00:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  startDate() {
+    time = this.startString.split(" ");
+    switch(time[0]) {
+      case "January":
+        month = 0;
+        break;
+      case "Jan":
+        month = 0;
+        break;
+      case ""
+
+    }
+    let d = new Date(time[2], time[0], time[1].split(",")[0])
+  }
+
+}
+
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+}
